6d97ef92442d1265b1140c940097b12181269eae
Priyanka_Hatgine <patil.priyanka885@gmail.com>
2021-05-21 18:18:41 +0530

    Added TestContainer Integration

--- src/main/java/com/springboot/demo/SpringBootApp.java
+++ src/main/java/com/springboot/demo/SpringBootApp.java
@@ -1,7 +1,8 @@
 package com.springboot.demo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@SpringBootConfiguration
 @SpringBootApplication
 public class SpringBootApp {
 	public static void main(String[] args) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT into employees(id,name,salary) VALUES (1,'Vardhaman Hatgine',20000)
+--INSERT into employees(id,name,salary) VALUES (6,'Vishal Patil',25000)
