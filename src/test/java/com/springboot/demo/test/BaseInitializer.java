6d012009b5b0f22f8b09c1e12f3a39d08aaed0a6
priyankahatgine885 <82951765+priyankahatgine885@users.noreply.github.com>
2021-05-21 18:21:07 +0530

    Added TestContainer Integration (#1)

---
+++ src/test/java/com/springboot/demo/test/BaseInitializer.java
@@ -0,0 +1,42 @@
+package com.springboot.demo.test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class BaseInitializer {
+    @Container
+    public static MySQLContainer container = new MySQLContainer("mysql:8.0")
+            .withDatabaseName("test_container_db")
+            .withPassword("priyanka")
+            //.withInitScript("container/INIT.sql")
+            .withUsername("priyanka");
+
+    static {
+        container.start();
+    }
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.username", container::getUsername);
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test() {
+        Assert.assertTrue(container.isRunning());
+    }
+}
--- src/test/java/com/springboot/demo/test/EmployeeControllerTest.java
+++ src/test/java/com/springboot/demo/test/EmployeeControllerTest.java
@@ -12,19 +12,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EmployeeControllerTest {
+public class EmployeeControllerTest extends  BaseInitializer {
     @Autowired
     private EmployeeController employeeController;
     @Autowired
     private EmployeeDao employeeDao;
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
+    @Sql("/data.sql")
     public void testCreateEmployee() {
         EmployeeDTO employeeDTO = new EmployeeDTO();
-        employeeDTO.setName("Sachin Patil111111");
+        employeeDTO.setName("Gomtesh");
         employeeDTO.setSalary(25000.0f);
         ResponseEntity<EmployeeResponse> response = employeeController.addEmployee(employeeDTO);
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
@@ -34,13 +40,14 @@ public class EmployeeControllerTest {
     @Test
     public void testDuplicateEmployeeNameNotAllowed() {
         EmployeeDTO employeeDTO = new EmployeeDTO();
-        employeeDTO.setName("Amit Patil1111");
+        employeeDTO.setName("Amit Patil11111");
         employeeDTO.setSalary(2222);
         ResponseEntity<EmployeeResponse> response = employeeController.addEmployee(employeeDTO);
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assert.assertEquals(response.getBody().getSuccess(), true);
 
     }
+
     @Test
     public void testListEmployees() throws Exception {
         ResponseEntity<EmployeeListResponse> response = employeeController.getEmployees();
@@ -48,19 +55,21 @@ public class EmployeeControllerTest {
         Assert.assertEquals(response.getBody().getSuccess(), true);
         Assert.assertNotNull(response.getBody().getEmployeeList());
     }
+
     @Test
     public void testUpdateEmployee() {
         Employee employee = new Employee();
-        employee.setName("Mrunali mali");
+        employee.setName("Dipali Patil");
         employee.setSalary(20000);
-        ResponseEntity<EmployeeResponse> response = employeeController.updateEmployee(9, employee);
+        ResponseEntity<EmployeeResponse> response = employeeController.updateEmployee(1, employee);
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assert.assertEquals(response.getBody().getSuccess(), true);
         Assert.assertEquals(response.getBody().getEmployee().getName(), employee.getName());
     }
+
     @Test
     public void testDeleteEmployee(){
-        Employee employee = employeeDao.findEmployeeByName("GOmtesh444");
+        Employee employee = employeeDao.findEmployeeByName("Gomtesh");
         ResponseEntity<EmployeeResponse> response = employeeController.deleteEmployee(employee.getId());
         Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assert.assertEquals(response.getBody().getSuccess(), true);
